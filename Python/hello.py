cb53afa3becaa4a2ef2333db23ecf6b830264568
Akash <akashindia572@gmail.com>
2025-11-16 11:34:49 +0530

    Single Line and Multi Lines comment.

--- Python/hello.py
+++ Python/hello.py
@@ -1,3 +1,11 @@
+# This is a single line comment in Python
+
+'''This is a multiline comment
+in python using triple quotes.'''
+
+
+# This is how we can print multiple lines in Python: using one single print statement.
+
 print('''
       Hello this is a way,
       to print multiline
