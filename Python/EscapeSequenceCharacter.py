2eae636898fb45ee828e606d456fdfd6cd23eaa8
Akash <akashindia572@gmail.com>
2025-11-18 16:10:08 +0530

    Escape Sequence Character in one go.

---
+++ Python/EscapeSequenceCharacter.py
@@ -0,0 +1,43 @@
+# \n   -(New Line)           This escape character moves the text after it to a new line.
+print("Hello\nWorld")
+
+
+
+# \t   -(Tab)           Inserts a horizontal tab space.
+print("Name:\tPython")
+
+
+
+# \\   -(Backslash)           Allows us to print a single backslash.
+print("This is a backslash: \\"
+
+)
+
+# \'   -(Single Quote)           Useful when writing single-quoted strings that contain apostrophes or another quote.
+
+print('It\'s a sunny day')
+
+
+
+# \"   -(Double Quote)           Useful when writing double-quoted strings that contain quotes.
+
+print("He said \"Hello!\"")
+
+
+
+# \r   -(Carriage Return)           Moves the cursor to the beginning of the line; text after it overwrites earlier text.
+
+print("Hello\rHi")  #Output may vary; "Hi" overwrites the beginning of "Hello"
+
+
+
+# \b   -(Backspace)           Deletes the previous character.
+print("Hellooo\b")  # Removes one "o"
+
+
+
+
+# \f   -(Form Feed)           Typically creates a page break; behavior differs across consoles.
+print("Hello\fWorld")
+
+
